d30493b3499170e191211f46b25e9522a2e7079c
Samuel Chávez <schavezfuentes@gmail.com>
2020-03-26 11:42:29 -0600

    Custom actions

--- ejemplos_django/example/pets/views.py
+++ ejemplos_django/example/pets/views.py
@@ -1,4 +1,6 @@
 from rest_framework import viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
 from pets.models import Pet
 from pets.serializers import PetSerializer
@@ -7,3 +9,34 @@ from pets.serializers import PetSerializer
 class PetViewSet(viewsets.ModelViewSet):
     queryset = Pet.objects.all()
     serializer_class = PetSerializer
+
+    @action(detail=True, methods=['post'])
+    def notify(self, request, pk=None):
+        pet = self.get_object()
+
+        # TODO: conectarme a FCM y mandar la push
+        print(pet.owner.name)
+
+        return Response({
+            'status': 'ok'
+        })
+
+    @action(detail=True, url_path='happy-bday', methods=['post'])
+    def happy_birthday(self, request, pk=None):
+        pet = self.get_object()
+
+        # TODO: conectarme a FCM y mandar la push
+        print("FELIZ CUMPLE MANO {}!".format(pet.name))
+        pet.age += 1
+        pet.save()
+
+        return Response(PetSerializer(pet).data)
+
+
+
+'''
+C reate
+R ead
+U pdate
+D elete
+'''

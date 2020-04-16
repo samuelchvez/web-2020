b0c7e2476b9cbe9eb092f867d0dc3d671fdf05d8
Samuel Chávez <schavezfuentes@gmail.com>
2020-04-16 17:36:05 -0600

    Cors headers

--- ejemplos_django/example/example/settings.py
+++ ejemplos_django/example/example/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
 
     'rest_framework',
     'guardian',
+    'corsheaders',
 
     'pets.apps.PetsConfig',
     'owners.apps.OwnersConfig',
@@ -50,6 +51,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -147,3 +149,5 @@ AUTHENTICATION_BACKENDS = (
     'django.contrib.auth.backends.ModelBackend',
     'guardian.backends.ObjectPermissionBackend'
 )
+
+CORS_ORIGIN_ALLOW_ALL = True
--- ejemplos_redux/src/sagas/trafficLights.js
+++ ejemplos_redux/src/sagas/trafficLights.js
@@ -1,19 +1,35 @@
 import {
-  // call,
+  call,
   takeEvery,
-  // put,
+  put,
   // race,
   // all,
-  // delay,
+  delay,
   // take,
 } from 'redux-saga/effects';
 
-// import * as actions from '../actions/instances';
+import * as actions from '../actions/trafficLights';
 import * as types from '../types/trafficLights';
 
 
 function* sayHappyBirthday(action) {
-  console.log("LOL! SI LLEGA!!", action)
+  const response = yield call(
+    fetch,
+    'http://localhost:8000/api/v1/token-auth/',
+    {
+      method: 'POST',
+      body: JSON.stringify({
+        username: 'douglas',
+        password: 'doug',
+      }),
+      headers:{
+        'Content-Type': 'application/json',
+      },
+    }
+  );
+
+  const result = yield response.json();
+  console.log(result);
 }
 
 export function* watchTrafficLightCreation() {
